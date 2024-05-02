f248c39d808765d1245b2ebc7dc243dca4446a82
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-02 18:34:45 +0000

    flexible route and action and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,17 @@ class PagesController < ApplicationController
   def home
     render ({ :template => "dice_pages/home"})
   end
+
+  def flexible
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+  
+    @num_dice.times do
+    die = rand(1..@sides)
+    @rolls.push(die)
+    end
+    render ({ :template => "dice_pages/flexible"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", {:controller => "pages", :action => "home"})
+
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "pages", :action => "flexible"})
 end
